3f45c73bebac360cfe562934655b5d54a839f9d3
Isa Setiawan <azaqokay@gmail.com>
2018-12-17 21:44:27 +0700

    list all users

--- src/components/users.js
+++ src/components/users.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
+import {Grid, Card} from 'semantic-ui-react'
 
 class Users extends Component{
+    constructor(props){
+        super(props)
 
+        this.state = {
+            users:[]
+        };
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> response.json())
+        .then(users => this.setState({users}))
+    }
 
 
     render(){
+        const { users } = this.state
         return (
-            <h1>List of users</h1>
+            <Grid stackable columns={5}>
+                {users.map(user=><Grid.Column><Card 
+                header={user.name} 
+                meta={user.email}
+                ></Card></Grid.Column>)}
+            </Grid>
         )
     }
 }
