40669dc493102e3d5ccd8285141f563535730cdc
Isa Setiawan <azaqokay@gmail.com>
2018-12-18 11:48:55 +0700

    add click handler

--- src/components/users.js
+++ src/components/users.js
@@ -4,7 +4,7 @@ import {Grid, Card} from 'semantic-ui-react'
 class Users extends Component{
     constructor(props){
         super(props)
-
+        this.selectUser = this.selectUser.bind(this)
         this.state = {
             users:[]
         };
@@ -16,16 +16,23 @@ class Users extends Component{
         .then(users => this.setState({users}))
     }
 
+    selectUser(id){
+        this.props.history.push('/'+id)
+    }
 
     render(){
         const { users } = this.state
         return (
-            <Grid stackable columns={5}>
-                {users.map(user=><Grid.Column><Card 
-                header={user.name} 
-                meta={user.email}
-                ></Card></Grid.Column>)}
-            </Grid>
+            <div>    
+                <Grid stackable columns={5}>
+                    {users.map(user=><Grid.Column><Card 
+                    key={user.id}
+                    onClick={() => this.selectUser(user.id)}
+                    header={user.name} 
+                    meta={user.email}
+                    ></Card></Grid.Column>)}
+                </Grid>
+            </div>
         )
     }
 }
