1290909c9d7261a9ac482fc27a99599d1f388e39
Isa Setiawan <azaqokay@gmail.com>
2018-12-18 15:01:35 +0700

    clear comment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-// import Home from './components/home'
 import Users from './components/users'
 import User from './components/user'
-// import Header from './components/header'
 import'./App.css'
 class App extends Component {
   render() {
