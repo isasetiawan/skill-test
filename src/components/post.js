c9b65e2103e42961e354bf1acd8f918fc81ce863
Isa Setiawan <azaqokay@gmail.com>
2018-12-18 21:36:07 +0700

    add ability to delete post

--- src/components/post.js
+++ src/components/post.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, Comment, Button } from 'semantic-ui-react';
+import { Card, Comment, Button, Modal } from 'semantic-ui-react';
 import CreateUpdatePost from './create-update-post';
 
 class Post extends Component {
@@ -7,16 +7,36 @@ class Post extends Component {
     constructor(props){
         super(props)
         this.state = {
+            modal_is_open:false,
             post:null,
             is_editting:false,
             comments:[]
         }
 
         this.toggleEdit = this.toggleEdit.bind(this)
+        this.deleteConfirm = this.deleteConfirm.bind(this)
+        this.doDelete = this.doDelete.bind(this)
     }
 
     toggleEdit(){
         this.setState({is_editting:!this.state.is_editting})
+    }
+
+    deleteConfirm(){
+        this.setState({modal_is_open:true})
+    }
+
+    doDelete(){
+        fetch(`https://jsonplaceholder.typicode.com/posts/${this.state.post.id}`, {
+                method: 'DELETE',
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+        .then(res=>{
+            if (res.status >= 200) this.setState({modal_is_open:false})
+            return res
+        })
     }
 
     componentDidMount(){
@@ -39,7 +59,9 @@ class Post extends Component {
                         <Card.Content>
                             <Card.Header>{post.title}</Card.Header>
                             <Card.Description>{post.body}</Card.Description>
-                            <Button floated="right" onClick={this.toggleEdit}>Update</Button>
+                            <Button floated="right" icon="trash" size="tiny" onClick={this.deleteConfirm} color="red"  ></Button>
+                            <Button floated="right" icon="edit" size="tiny" onClick={this.toggleEdit} ></Button>
+
                         </Card.Content>
                         <Card.Content extra>
                             <Comment.Group>
@@ -56,6 +78,18 @@ class Post extends Component {
                             </Comment.Group>
                         </Card.Content>
                     </Card>
+                    
+                    {/* modal to confirm deletion of the post */}
+                    <Modal size='mini' open={this.state.modal_is_open} onClose={() => this.setState({modal_is_open:false})}>
+                        <Modal.Header>Delete Your Account</Modal.Header>
+                        <Modal.Content>
+                            <p>Are you sure you want to delete your account</p>
+                        </Modal.Content>
+                        <Modal.Actions>
+                            <Button negative onClick={() => this.setState({modal_is_open:false})}>No</Button>
+                            <Button positive onClick={this.doDelete} icon='checkmark' labelPosition='right' content='Yes' />
+                        </Modal.Actions>
+                    </Modal>
                 </div>
             )
         } else {
