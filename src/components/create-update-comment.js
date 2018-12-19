616d0714961b9935c5e8c204fa5c70538c70451f
Isa Setiawan <azaqokay@gmail.com>
2018-12-19 14:57:54 +0700

    component to create and update comment

---
+++ src/components/create-update-comment.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { Form, Button } from 'semantic-ui-react'
+
+class CreateUpdateComment extends Component {
+
+    constructor(props){
+        super(props)
+
+        this.state={
+            ...props.comment,
+            is_loading : false
+        }
+        this.handleInputChange = this.handleInputChange.bind(this)
+        this.submitComment = this.submitComment.bind(this)
+    }
+
+    submitComment(){
+        let {name, email, body, id} = this.state;
+        let postId = this.props.match.params.postId
+        
+        this.setState({is_loading:true})
+
+        fetch(`https://jsonplaceholder.typicode.com/comments/${id?id:''}`, {
+                method: id ? 'PUT' : 'POST',
+                body: JSON.stringify({name, email, body, postId}),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+        .then(res=>{
+            if (res.status >= 200){
+                window.location.reload()
+            }
+            this.setState({is_loading:true})
+            return res
+        })
+        .then(response => response.json())
+        .then(json => console.log(json))
+    }
+
+    handleInputChange(event){
+        const target = event.target
+        const name = target.name
+        this.setState({
+            [name]:target.value
+        })
+    }
+
+    render(){
+        let {name, email, body, is_loading } = this.state;
+        return (
+            <Form reply onSubmit={this.submitComment}>
+                <Form.Input  onChange={this.handleInputChange} name="name" value={name}  placeholder="Name"></Form.Input>
+                <Form.Input  onChange={this.handleInputChange} name="email" value={email}  placeholder="E-mail" type="email"></Form.Input>
+                <Form.TextArea  onChange={this.handleInputChange} name="body" value={body}  placeholder="write your comment here" />
+                <Button loading={is_loading} content='Submit Comment' labelPosition='left' icon='edit' primary />
+            </Form>
+        )
+    }
+}
+
+export default CreateUpdateComment
